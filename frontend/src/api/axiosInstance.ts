7f0ef83685edb7e1a333fbe7b3a19e2f1a95a981
Alabiabdullah1 <Alabiabdullah9@gmail.com>
2025-06-03 08:22:08 +0100

    Api route update

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_URL= "https://finance-tracker-backend-zxc8.onrender.com"
 
-const API_URL = "http://localhost:5000";
+
 
 const instance = axios.create({
     baseURL: API_URL + "/api",
